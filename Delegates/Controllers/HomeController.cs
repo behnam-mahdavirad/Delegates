4dd09c484a1b976cb3388e09c05f7b68ee3448a1
Behnam Mahdavirad <behnam.mahdavirad@hotmail.com>
2019-01-04 10:17:52 +0330

    enhancement

--- Delegates/Controllers/HomeController.cs
+++ Delegates/Controllers/HomeController.cs
@@ -11,8 +11,22 @@ namespace Delegates.Controllers
         // GET: Home
         public ActionResult Index()
         {
-            return View();
+            MyDelegate del = new MyDelegate(myMethod1);
+            del += new MyDelegate(myMethod2);
+            return Content("<h1>" + del("value: ") + "</h1>");
         } // end ActionResult Index GET
+
+        delegate string MyDelegate(string argument);
+
+        private string myMethod1(string msg)
+        {
+            return msg + "I am in myMethod1";
+        } // end method myMethod1
+
+        private string myMethod2(string msg)
+        {
+            return msg + "I am in myMethod2";
+        } // end method myMethod2
 
     } // end class HomeController : Controller
 
