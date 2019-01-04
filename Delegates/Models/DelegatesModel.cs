ca64830ce0c3531140487f053f290ed4ee12aeae
Behnam Mahdavirad <behnam.mahdavirad@hotmail.com>
2019-01-04 09:54:22 +0330

    Add project files.

---
+++ Delegates/Models/DelegatesModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Delegates.Models
+{
+    public class DelegatesModel
+    {
+
+    } // end class DelegatesModel
+
+} // end namespace Delegates.Models
